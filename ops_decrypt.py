ffbb982aed9b35bea2183a86ab561aa1aaade190
Bjoern Kerler <6317772+bkerler@users.noreply.github.com>
2019-06-04 22:43:20 +0200

    Add support for OP6 + OP7
    Usage: ./ops_decrypt.py "MSMDownloadTool V4.0.exe"
    Then start regular flash procedure. It will save all decrypted files as decrypted.data[x].
    For Oneplus 7 Pro, decrypted.data0 contains the payload.xml, and decrypted.data1 contains the firehose loader

--- ops_decrypt.py
+++ ops_decrypt.py
@@ -2,15 +2,17 @@ from __future__ import print_function
 import frida
 import sys
 import os
-
+i=0
 def on_message(message, data):
+    global i
     if "payload" in message:
         if (message["payload"]=="Output"):
-            if (os.path.exists("decrypted.data")):
-                os.remove("decrypted.data")
-            with open("decrypted.data","wb") as wf:
+            if os.path.exists("decrypted.data"+str(i)):
+                os.remove("decrypted.data"+str(i))
+            with open("decrypted.data"+str(i),"wb") as wf:
                 wf.write(data)
-                print("Decrypted data was written to 'decrypted.data'.")
+                print("Decrypted data was written to 'decrypted.data"+str(i)+"'.")
+            i+=1
     else:
         print("[%s] => %s" % (message, data))
 
@@ -23,7 +25,7 @@ def main(target_process, start, length):
     var processname="";
     
     var op = recv('param', function(value) {
-                v = value.payload.split(",");
+                var v = value.payload.split(",");
                 seekoffset = v[0];
                 seeklength = v[1];
                 processname = v[2];
@@ -32,12 +34,12 @@ def main(target_process, start, length):
 
     function run()
     {
-        proc=Module.findBaseAddress(processname);
-        base=Process.findRangeByAddress(proc);
-        fileptr=Module.findExportByName("kernel32","SetFilePointer");
-        readfileptr=Module.findExportByName("kernel32","ReadFile");
+        var proc=Module.findBaseAddress(processname);
+        var base=Process.findRangeByAddress(proc);
+        var fileptr=Module.findExportByName("kernel32","SetFilePointer");
+        var readfileptr=Module.findExportByName("kernel32","ReadFile");
         
-        Memory.scan(base.base, 0x100000, "55????5D????FFFFFF",
+        /*Memory.scan(base.base, 0x100000, "55????5D????FFFFFFE9",
         { 
             onMatch: function(address,size) 
             {  
@@ -45,12 +47,13 @@ def main(target_process, start, length):
                 
                 Interceptor.attach(ptr(address), { 
                             // When function is called, print out its parameters
+                            
                             onEnter: function (args) {
-                                
+                                console.log("Entered at :"+address.toString(16))
                                 this.length=args[0].toInt32();
-                                if (args[0]<0x20000)
+                                if (args[0]<0x16000)
                                 {
-                                    subsize=args[0]
+                                    var subsize=args[0]
                                     Interceptor.attach(readfileptr, { 
                                         onEnter: function (args) {
                                             console.log('');
@@ -78,15 +81,16 @@ def main(target_process, start, length):
             onComplete: function() 
             { 
             } 
-        });
+        });*/
         
         Memory.scan(base.base, 0x100000, "D1B5E39E",
         { 
             onMatch: function(address,size) 
             {  
+                var z=0;
                 for (z=address-50;z<address;z++) 
                 { 
-                    h=Memory.readU8(ptr(z));
+                    var h=Memory.readU8(ptr(z));
                     if (h==0x55)
                     {
                         console.log("Detected AES: "+ptr(z).toString(16));
@@ -103,7 +107,7 @@ def main(target_process, start, length):
                             // When function is finished
                             onLeave: function (retval) {
                                 dumpAddr('Data', this.xx, 16); // Print out data array, which will contain de/encrypted data as output
-                                dt=Memory.readByteArray(this.xx,this.length);
+                                var dt=Memory.readByteArray(this.xx,this.length);
                                 send('Output',dt);
                                 console.log("Writing data.");
                             }
@@ -118,7 +122,7 @@ def main(target_process, start, length):
             } 
         });
 
-        Interceptor.attach(fileptr, { 
+        /*Interceptor.attach(fileptr, { 
                             onEnter: function (args) {
                                 console.log('');
                                 if (args[1]>0x0 && args[1]<0xFFFF0000)
@@ -133,7 +137,7 @@ def main(target_process, start, length):
                             onLeave: function (retval) {
                                 
                             }
-                        });
+                        });*/
     }
     
                     
@@ -157,9 +161,9 @@ def main(target_process, start, length):
     session.detach()
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print ("Oppo MSMDownloadTool V4.0 Firmware decrypter (c) B.Kerler 2017\n") 
-        print ("Usage: %s <process name:MSMDownloadTool.exe> <start offset:0x0> <length:0x100000>" % __file__)
+    if len(sys.argv) != 2:
+        print ("Oppo MSMDownloadTool V4.01 Firmware decrypter (c) B.Kerler 2017-2019\n") 
+        print ("Usage: %s <process name:MSMDownloadTool.exe>" % __file__)
         sys.exit(1)
 
     try:
